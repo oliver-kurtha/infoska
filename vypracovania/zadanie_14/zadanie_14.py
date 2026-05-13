be198d7df69386ac34feb2b83f3d0245234a3266
oliver-kurtha <oliverkurtha@gmail.com>
2026-05-13 20:31:30 +0200

    sample 14

---
+++ vypracovania/zadanie_14/zadanie_14.py
@@ -0,0 +1,5 @@
+# Zadanie 14
+
+# V textovom súbore sú uložené trasy liniek mestskej hromadnej dopravy. V jednotlivých riadkoch je zaznamenaná trasa linky v podobe postupnosti jednotlivých zastávok, ktorými linka prechádza. Zisti koľko liniek jazdí v meste a ktorá linka má najdlhšiu trasu. Pre všetky zastávky v meste zisti koľko liniek cez ne prechádza. (mhd.txt)
+
+f = open()
